9041cb5286de797e1eca85f4f673a6b6f1de50ca
adatun <adatunaaltinci@ogrenci.beykoz.edu.tr>
2025-10-19 22:12:53 +0300

    Add files via upload

---
+++ dersiçinkodlamaödev.cs
@@ -0,0 +1,29 @@
+int dersnotlari = 25;
+switch (dersnotlari / 10)
+{
+    case 10:
+    case 9:
+        Console.WriteLine("aa");
+        break;
+    case 8:
+        Console.WriteLine("ba");
+        break;
+    case 7:
+        Console.WriteLine("bb");
+        break;
+    case 6:
+        Console.WriteLine("cb");
+        break;
+    case 5:
+        Console.WriteLine("cc");
+        break;
+    case 4:
+        Console.WriteLine("dc");
+        break;
+    case 3:
+        Console.WriteLine("dd");
+        break;
+    default:
+        Console.WriteLine("Dersten Kaldınız");
+        break;
+}
